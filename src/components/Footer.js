b93223b83a7c392bbf8a0eb9db873dc17f0704e9
vitorbrambilla <vitorbrambilla10@gmail.com>
2022-05-16 10:56:07 -0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+// CSS
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h3>Escreva sobre o que você tem interesse!</h3>
+      <p>Mini Blog &copy; <strong>2022</strong></p>
+    </footer>
+  )
+}
+
+export default Footer
