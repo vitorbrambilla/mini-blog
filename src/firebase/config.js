dbe2ea27703af29d1107656282798fdcfb1841fc
vitorbrambilla <vitorbrambilla10@gmail.com>
2022-05-16 11:30:33 -0300

    changes on config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqhXlJzUhokPna7W-1fxReboyRHI7cqJw",
-  authDomain: "mini-blog-ab784.firebaseapp.com",
-  projectId: "mini-blog-ab784",
-  storageBucket: "mini-blog-ab784.appspot.com",
-  messagingSenderId: "1009875361653",
-  appId: "1:1009875361653:web:265e17d0fcfedac548fd3c"
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: 
 };
 
 const app = initializeApp(firebaseConfig);
